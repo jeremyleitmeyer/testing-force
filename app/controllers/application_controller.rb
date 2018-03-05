9f404f2c2a233d0c305c7ab13edb91cddf6001ac
jeremyleitmeyer <jeremyleitmeyer@Jeremys-MacBook-Air.local>
2018-03-05 15:48:24 -0500

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  ;akshdf;lkhasldkfj;laksjdf;lkajsdl;kfja;lskdjf;laskjdfl;kajsdl;kfjals;dkfj;ldskfjl;skj
 end
